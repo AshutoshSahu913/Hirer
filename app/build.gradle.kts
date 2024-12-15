94eece2a81c49d6a695d2f9b749b1e499b5df0b1
Ashutosh Sahu <1ashutoshsahu@gmail.com>
2024-12-15 20:50:49 +0530

    add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    id("kotlin-parcelize")
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.job.hirer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.job.hirer"
@@ -39,14 +44,12 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
 }
 
 dependencies {
@@ -66,4 +69,43 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    //retrofit
+    implementation(libs.retrofit)
+    //moshi
+    implementation(libs.moshi.kotlin)
+    implementation(libs.converter.moshi)
+
+    //dagger hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //hilt viewmodel
+    implementation(libs.androidx.hilt.lifecycle.viewmodel)
+
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    //ViewModels
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.savedstate)
+
+    //coil for image loading
+    implementation(libs.coil.compose)
+
+
+    //data store pref
+    implementation (libs.androidx.datastore.preferences)
+
 }
---
+++ app/src/main/java/com/job/hirer/comman/Result.kt
@@ -0,0 +1,5 @@
+package com.job.hirer.comman
+
+
+class Result {
+}
